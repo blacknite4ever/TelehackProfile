4e14fa1511fb09bc0d88751423f715ef0124a14a
blacknite4ever <96452550+blacknite4ever@users.noreply.github.com>
2025-08-09 00:52:23 -0700

    Update plan.js

--- plan.js
+++ plan.js
@@ -33,8 +33,7 @@ for (let i = 0; i < columns; i++) {
 function draw() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
   ctx.fillRect(0, 0, width, height);
-  ctx.fillStyle = '#f00'; // Set the rain color to red
-  ctx.fillStyle = 'rgba(23, 207, 93, 0.3)';
+  ctx.fillStyle = '#17cf5d'; // Set the rain color to red
   ctx.font = `${fontSize}px monospace`;
 
   for (let i = 0; i < drops.length; i++) {
